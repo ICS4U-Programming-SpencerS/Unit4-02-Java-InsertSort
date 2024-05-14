721dd2c189f3755f43f06717ff44cfffd1fde5bd
PencersGitHub <113054133+PencersGitHub@users.noreply.github.com>
2024-05-14 10:48:56 -0400

    Initial commit

---
+++ com/example/InsertSort.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Spencer Scarlett
+ * @version 1.0
+ * @since 2024-05-14
+ */
+
+// InsertSort class
+public final class InsertSort {
+
+    /** Private constructor to prevent instantiation. */
+    private InsertSort() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
